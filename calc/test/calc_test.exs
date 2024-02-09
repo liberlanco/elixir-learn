aedc3e1e4b08595117da8cbf108ee8ce0b551ed0
Artem <art@localhost>
2024-02-09 13:19:03 +0300

    Add docs, turn on tests

--- calc/test/calc_test.exs
+++ calc/test/calc_test.exs
@@ -1,13 +1,4 @@
 defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
-
-  test "tokenize should parse strings" do
-    assert Calc.Parser.tokenize("1+2") == {:ok, [1.0, :add, 2.0]}
-    assert Calc.Parser.tokenize("1 +    2") == {:ok, [1.0, :add, 2.0]}
-    assert Calc.Parser.tokenize("1.0 + 2e") == {:error, "failed to parse token 2e: extra chars"}
-    assert Calc.Parser.tokenize("1-2") == {:ok, [1.0, :sub, 2.0]}
-    assert Calc.Parser.tokenize("1*2/2") == {:ok, [1.0, :mul, 2.0, :div, 2.0]}
-    assert Calc.Parser.tokenize("-2+2") == {:ok, [:sub, 2.0, :add, 2.0]}
-  end
 end

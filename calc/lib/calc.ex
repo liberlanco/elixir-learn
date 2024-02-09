dbdc0543df350cebbd6b8afee06373f41e6e0f69
Artem <art@localhost>
2024-02-09 13:43:20 +0300

    Quick final test, update doc, catch common wrong case. Done

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -2,6 +2,10 @@ defmodule Calc do
   @moduledoc """
   Main interfaces for calculator.
 
+  It's a simple calculator, no support for operation priorities, no sofisticated error handling
+  of wrong input, no support for brackets and other complex things like negative numbers in the
+  middle of input
+
   Use `eval` in code as API. And use `calculate` as
   human interfaces with IO.puts
   """
@@ -19,6 +23,12 @@ defmodule Calc do
 
       iex> Calc.eval("2+2")
       {:ok, 4.0}
+
+      iex> Calc.eval("2 + 2 * 2e10")
+      {:ok, 80000000000.0}
+
+      iex> Calc.eval("2 / 0")
+      ** (ArithmeticError) bad argument in arithmetic expression
   """
   def eval(exp) do
     with {:ok, tree} <- Calc.Parser.parse(exp),
